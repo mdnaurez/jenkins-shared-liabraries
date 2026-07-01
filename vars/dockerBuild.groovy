f1c7d834329a45a2b9b03afbd86289722de8a113
mdnaurez <117558398+mdnaurez@users.noreply.github.com>
2026-07-01 12:25:31 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,3 +1,4 @@
 def call(String projectName, String ImageTag, String DockerHubUser ){
-  bat 'docker build -t ${DockerHubUSer}/${projectName}:${ImageTag} .'
+  //bat 'docker build -t ${DockerHubUSer}/${projectName}:${ImageTag} .'
+  bat "docker build -t ${DockerHubUser}/${projectName}:${ImageTag} ."
 }
