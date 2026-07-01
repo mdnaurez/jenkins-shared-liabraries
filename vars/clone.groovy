5547f7263f00639a52700c3ea139c1f65c317af9
mdnaurez <117558398+mdnaurez@users.noreply.github.com>
2026-07-01 10:26:01 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,6 +1,7 @@
 def call(String url, String branch){
   echo 'cloning the code'
   //git url: url, branch:branch
+  //or
   git url: "${url}", branch: "${branch}"
   echo 'code cloned successfully'
 }
